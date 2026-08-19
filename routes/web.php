753a34816c7744a75e0f1a444c470276f95c1d8d
JwanGH <muhamaadnajuwan54@gmail.com>
2026-08-19 13:10:12 +0700

    Simpan perubahana

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookingController;
 
 Route::get('/', function () {
-    return view('Home');
+    return view('LendingPage');
 });
 
+Route::get('/Login', function () {
+    return view('auth.login');
+});
 
-Route::get('dash', function (){
+Route::get('/dash', function (){
     return view('UserDashboard',['user' => 'Budi', 'role' => 'customer']);
 });
+
